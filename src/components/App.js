6e1af02b02f913af70a3038a785e308ec391d40c
Shankar-pathade <109196454+Shankar-pathade@users.noreply.github.com>
2023-02-26 16:09:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,29 @@ const App = () => {
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
 
+  
+  const start = ()=> {
+    intervalRef.current = setInterval(() => {
+      currentTime((time) => time*10)
+    }, 10);
+  };
+  
+   const stop = ()=> {
+    clearInterval(intervalRef.current);
+  };
+  
+   const reset = ()=> {
+    clearInterval(intervalRef.current);
+     startTime(
+     setLaps([]);
+  };
   return (
     <div id="main">
       <section>
         <h1 className='seconds-elapsed'>Stopwatch Time</h1>
         <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
+          <button className="start-btn" onClick={start}>START</button>
+          <button className="stop-btn"  onClick={stop}>STOP</button>
           <button className="lap-btn">LAP</button>
           <button className="reset-btn">RESET</button>
         </section>
